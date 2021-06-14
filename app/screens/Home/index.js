74545abcfd88aab2650063840be2ce9280c33916
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-06-14 19:34:54 +0200

    ✨ Add home screen

---
+++ app/screens/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+const Home = () => {
+
+  return (
+    <View>
+      <Text>Home screen</Text>
+    </View>
+  );
+};
+
+export default Home;
