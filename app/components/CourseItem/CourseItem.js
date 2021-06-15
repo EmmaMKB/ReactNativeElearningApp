9b4d1e7fdbfad243e1a4b364f0e30135cb6f889e
Emmanuel Makabu <emmanuelmakabu7@gmail.com>
2021-06-15 10:40:02 +0200

    ✨ Add course item component

---
+++ app/components/CourseItem/CourseItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+} from 'react-native';
+
+import { theme } from '../../constants';
+
+const { COLORS, SIZES } = theme;
+
+const CourseItem = () => {
+
+  return (
+    <View style={styles.container}>
+        
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+   
+})
+export default CourseItem;
